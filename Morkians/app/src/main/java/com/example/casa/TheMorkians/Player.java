e84af34fe736e5b7323057d27473e6b783e68767
pedroabgmarques <pedroabgmarques@gmail.com>
2015-12-28 21:53:09 +0000

    Lixo removido da classe Player

--- Morkians/app/src/main/java/com/example/casa/TheMorkians/Player.java
+++ Morkians/app/src/main/java/com/example/casa/TheMorkians/Player.java
@@ -1,24 +1,9 @@
 package com.example.casa.TheMorkians;
 
-import android.app.Activity;
-
-import org.andengine.engine.options.EngineOptions;
-import org.andengine.entity.modifier.ScaleAtModifier;
-import org.andengine.entity.scene.IOnSceneTouchListener;
-import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
-import org.andengine.entity.sprite.vbo.ISpriteVertexBufferObject;
-import org.andengine.input.touch.TouchEvent;
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
-import org.andengine.opengl.texture.region.TextureRegion;
-import org.andengine.opengl.vbo.VertexBufferObject;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
-import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-import java.io.IOException;
-import java.util.Vector;
 
 /**
  * Created by Victor on 26/12/2015.
