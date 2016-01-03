bb48c9dd4754166510b1af0a321573b44adeab8d
paulodgn <paulo.nwork@gmail.com>
2016-01-03 23:19:37 +0000

    Inimigos disparam balas. Não sao criadas no sitio certo, nao tem velocidade propria, nao tem direção certa.

--- Morkians/app/src/main/java/com/example/casa/TheMorkians/Enemy.java
+++ Morkians/app/src/main/java/com/example/casa/TheMorkians/Enemy.java
@@ -1,5 +1,7 @@
 package com.example.casa.TheMorkians;
 
+import org.andengine.engine.handler.timer.ITimerCallback;
+import org.andengine.engine.handler.timer.TimerHandler;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
@@ -10,15 +12,33 @@ import org.andengine.opengl.vbo.VertexBufferObjectManager;
 public class Enemy extends Sprite
 {
     private int vida;
-    public int positionX;
+    public int positionX, positionY;
+    private Bala novaBala;
+    ResourcesManager resourcesManager;
 
-    public Enemy(float pX, float pY, ITextureRegion pTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager)
+    public Enemy(float pX, float pY, ITextureRegion pTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager, ResourcesManager rManager)
     {
         super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
 
         vida=2;
         positionX=(int)pX;
+        positionY=(int)pY;
+        resourcesManager=rManager;
+        shoot();
     }
 
+    public void shoot()
+    {
+        TimerHandler timerHandler = new TimerHandler(3, true,
+                new ITimerCallback() {
+                    @Override
+                    public void onTimePassed(TimerHandler pTimerHandler) {
+                        //criar nova bala.
+                        novaBala=new Bala(getX(), getY(), resourcesManager.gameEnemyLaserRegion,getVertexBufferObjectManager());
+                        attachChild(novaBala);
+                    }
+                });
+        registerUpdateHandler(timerHandler);
+    }
 
 }
